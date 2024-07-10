27c05d9d0c48236049bf41429f561f5c703953da
EL96cpp <el96cpp@gmail.com>
2024-07-10 19:44:14 +0300

    Added underline on hover for the main page references

--- static/js/jquery-ajax.js
+++ static/js/jquery-ajax.js
@@ -2,4 +2,22 @@ $(document).ready(function () {
 
     console.log("Hello, JazzWorld!");
 
+    $("#musicians-ref").mouseover(function(){
+
+        console.log("musicians ref!");
+
+    });
+
+    $("#styles-ref").mouseover(function(){
+
+        console.log("styles ref!");
+
+    });
+
+    $("#clubs-ref").mouseover(function(){
+
+        console.log("clubs ref!");
+
+    });
+
 });
