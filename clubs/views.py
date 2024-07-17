765f42d5a967de554131a47e5345ddf7f7790ac2
EL96cpp <el96cpp@gmail.com>
2024-07-17 16:36:15 +0300

    Added clubs.json fixture, added list of distinct 'city' and 'country' values to clubs view-function to filter clubs

--- clubs/views.py
+++ clubs/views.py
@@ -5,12 +5,11 @@ from .models import Club
 def clubs(request):
     clubs = Club.objects.all()
 
-    countries = Club.objects.order_by().values_list('country').distinct()
-    cities = Club.objects.order_by().values_list('city').distinct()
+    countries = [country[0] for country in Club.objects.order_by().values_list('country').distinct()]
+    cities = [city[0] for city in Club.objects.order_by().values_list('city').distinct()]
 
-    print(countries, cities)
+    print(countries, type(cities))
 
-    return render(request, 'clubs/clubs_list.html', {"clubs": clubs})
+    return render(request, 'clubs/clubs_list.html', {"clubs": clubs, "countries": countries, "cities": cities})
 
     
-
